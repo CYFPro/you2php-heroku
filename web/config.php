b6bfec3d08387c2cb8f6f8d6b61929b905cfdde1
CYFPro <41472804+CYFPro@users.noreply.github.com>
2018-10-01 15:00:11 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -2,7 +2,7 @@
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyB__U_fY6X_VOUUfIzhOvqMMCLCMnErW1Y');
 define('GJ_CODE', 'JP');//国家代码
-define('SITE_NAME', 'Pig's oil Youtube');//网站名字
+define('SITE_NAME', 'Pigs oil Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF24232trgdsds');//一个随机字符串，加解密KEY
 define('EMAIL', 'a2778754364@gmail.com');//邮箱
